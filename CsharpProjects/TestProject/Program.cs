c10ef760755b362f3d0eaa194958cd314ecef4d8
mandi andrade <mandi.andrade20@gmail.com>
2024-01-14 15:18:19 -0600

    largerValue exercise completed

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,5 @@
-﻿Random dice = new Random();
+﻿// Exercise - Next method
+Random dice = new Random();
 int roll0 = dice.Next(1, 7);
 int roll1 = dice.Next();
 int roll2 = dice.Next(101);
@@ -8,3 +9,10 @@ Console.WriteLine("You rolled a: " + roll0);
 Console.WriteLine($"First roll: {roll1}");
 Console.WriteLine($"Second roll: {roll2}");
 Console.WriteLine($"Third roll: {roll3}");
+
+// Exercise - Math class methods
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
