d047582e51b5f5b6f67b820971266a3854d6d1a8
mandi andrade <mandi.andrade20@gmail.com>
2024-01-15 22:20:12 -0600

    practice with decision logic

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,14 +1,12 @@
-﻿// Exercise - Next method
+﻿// Exercise - Decision Logic Game
 Random dice = new Random();
-int roll0 = dice.Next(1, 7);
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-Console.WriteLine("You rolled a: " + roll0);
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 // Exercise - Math class methods
 int firstValue = 500;
