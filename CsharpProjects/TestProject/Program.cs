6eaa27f1b8f9d3e3a6afa6e25c988f6c36e41527
mandi andrade <mandi.andrade20@gmail.com>
2024-01-16 14:14:01 -0600

    updated game mechanics for prize options

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -3,9 +3,6 @@ Random dice = new Random();
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
-roll1 = 5;
-roll2 = 5;
-roll3 = 5;
 int total = roll1 + roll2 + roll3;
 
 // Intial total:
@@ -17,23 +14,32 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
   if ((roll1 == roll2) && (roll2 == roll3))
   {
     total += 6;
-    Console.WriteLine($"Congrats, you rolled a triple! +6 to your score: {total}");
+    Console.WriteLine("Congrats, you rolled a triple! +6 to your score.");
   }
   else
   {
     total += 2;
-    Console.WriteLine($"Congrats, you rolled doubles! +2 added to your score: {total}");
+    Console.WriteLine("Congrats, you rolled doubles! +2 added to your score.");
   }
+  Console.WriteLine($"Your modified score after bonuses: {total}");
 }
 
-// final score:
-if (total >= 15)
+// final score & prize announcement:
+if (total >= 16)
 {
-  Console.WriteLine("You win!");
+  Console.WriteLine("Woah, congrats! You win a brand new car!");
+}
+else if (total >= 10)
+{
+  Console.WriteLine("Nice roll; you win a new cell phone");
+}
+else if (total == 7)
+{
+  Console.WriteLine("Lucky 7; you win a free vacation!");
 }
 else
 {
-  Console.WriteLine("Sorry, you lose.");
+  Console.WriteLine("Thanks for playing; you win a donut!");
 }
 
 // Exercise - Math class methods
