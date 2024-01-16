a3905d6d8669042152d307d4adc6fbea51df6a2d
mandi andrade <mandi.andrade20@gmail.com>
2024-01-16 14:04:25 -0600

    refactor with conditional logic - else

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -8,16 +8,19 @@ int total = roll1 + roll2 + roll3;
 // Intial total:
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-// Check for triples and then doubles:
-if ((roll1 == roll2) && (roll2 == 3))
-{
-  total +=6;
-  Console.WriteLine($"Congrats, you rolled a triple! +6 to your score: {total}");
-}
+// Check for doubles and then triples, without Point Stacking:
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-  total += 2;
-  Console.WriteLine($"Congrats, you rolled doubles! +2 added to your score: {total}");
+  if ((roll1 == roll2) && (roll2 == roll3))
+  {
+    total += 6;
+    Console.WriteLine($"Congrats, you rolled a triple! +6 to your score: {total}");
+  }
+  else
+  {
+    total += 2;
+    Console.WriteLine($"Congrats, you rolled doubles! +2 added to your score: {total}");
+  }
 }
 
 // final score:
@@ -25,8 +28,7 @@ if (total >= 15)
 {
   Console.WriteLine("You win!");
 }
-
-if (total < 15)
+else
 {
   Console.WriteLine("Sorry, you lose.");
 }
