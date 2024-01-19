6cbd38c03deea4e6a9e7a5ced11736c418137373
mandi andrade <mandi.andrade20@gmail.com>
2024-01-18 18:54:03 -0600

    practice readable code using whitespace

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -140,12 +140,10 @@ foreach (string ID in orderIDs)
 */
 
 // Exercise 5 - Practice writing Readable Code
-/*
-  The following code creates five random OrderIDs
-  to test the fraud detection process. OrderIDs
-  consist of a letter from A to E, and a three 
-  digit number. Ex. A123.
-*/
+  // The following code creates five random OrderIDs
+  // to test the fraud detection process. OrderIDs
+  // consist of a letter from A to E, and a three 
+  // digit number. Ex. A123.
 Random random = new Random();
 string[] orderIDs = new string[5];
 for (int i = 0; i < orderIDs.Length; i++)
@@ -158,4 +156,40 @@ for (int i = 0; i < orderIDs.Length; i++)
 foreach (var orderID in orderIDs)
 {
   Console.WriteLine(orderID);
+}
+
+// Practice using whitespace for readable code:
+  // Example 1:
+Console
+.
+WriteLine
+(
+  "Hello example 1!"
+)
+;
+
+  // Example 2:
+string firstWord="Hello";string lastWord="example 2!";Console.WriteLine(firstWord+" "+lastWord);
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } 
+    else 
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
