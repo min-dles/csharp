1229d6aee8ac679efb632c8bf9e2147987d84884
mandi andrade <mandi.andrade20@gmail.com>
2024-01-23 13:38:30 -0600

    nested foreach loops to iterate thru all studentScores

--- CsharpProjects/GuidedProject/Starter/Program.cs
+++ CsharpProjects/GuidedProject/Starter/Program.cs
@@ -10,26 +10,43 @@ int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
+int[] studentScores = new int[10];
+
+// Write the report header to the console
+Console.WriteLine("Student\t\tGrade\n");
+
 foreach (string name in studentNames)
 {
-  Console.WriteLine($"Current student: {name}");
-  if (name == "Sophia")
+  string currentStudent = name;
+
+  if (currentStudent == "Sophia")
+    studentScores = sophiaScores;
+
+  else if (currentStudent == "Andrew")
+    studentScores = andrewScores;
+
+  else if (currentStudent == "Emma")
+    studentScores = emmaScores;
+
+  else if (currentStudent == "Logan")
+    studentScores = loganScores;
+
+  // initialize/reset the sum of scored assignments
+  int sumAssignmentScores = 0;
+
+  // initialize/reset the calculated average of exam + extra credit scores
+  decimal currentStudentGrade;
+
+  foreach (int score in studentScores)
   {
-    int sophiaSum = 0;
-
-    decimal sophiaScore;
-
-    foreach (int score in sophiaScores)
-    {
-      sophiaSum += score;
-    }
-
-    sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-    Console.WriteLine("Student\t\tGrade\n");
-    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+    sumAssignmentScores += score;
   }
+
+  currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+  Console.WriteLine($"{currentStudent}:\t\t{currentStudentGrade}\t?");
 }
+
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
