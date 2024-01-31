3e23385afe75c3b9749df20d46af262189c5797a
mandi andrade <mandi.andrade20@gmail.com>
2024-01-31 15:33:35 -0600

    set up variable scope challenge

--- CsharpProjects/Part3/TestProject/Program.cs
+++ CsharpProjects/Part3/TestProject/Program.cs
@@ -158,3 +158,29 @@ else if (name2 == "lola")
   Console.WriteLine("Found Lola");
 else
   Console.WriteLine("Found Ximena");
+
+// set up Variable Scope Challenge:
+Console.WriteLine("*** VARIABLE SCOPE CHALLENGE: ***");
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
+{
+    int total;
+
+    total += number;
+
+    if (number == 42)
+    {
+       bool found = true;
+
+    }
+
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
