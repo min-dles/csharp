1ab2f7530f32228aa5f827e587b49b67e45d03e8
mandi andrade <mandi.andrade20@gmail.com>
2024-01-31 15:43:10 -0600

    refactored code for challenge with successful output

--- CsharpProjects/Part3/TestProject/Program.cs
+++ CsharpProjects/Part3/TestProject/Program.cs
@@ -160,27 +160,23 @@ else
   Console.WriteLine("Found Ximena");
 
 // set up Variable Scope Challenge:
+  // Output should read as follows
+    // Set contains 42
+    // Total: 108
 Console.WriteLine("*** VARIABLE SCOPE CHALLENGE: ***");
 int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
 
 foreach (int number in numbers)
 {
-    int total;
+  total += number;
 
-    total += number;
-
-    if (number == 42)
-    {
-       bool found = true;
-
-    }
-
+  if (number == 42)
+    found = true;
 }
 
-if (found) 
-{
-    Console.WriteLine("Set contains 42");
-
-}
+if (found)
+  Console.WriteLine("Set contains 42");
 
 Console.WriteLine($"Total: {total}");
