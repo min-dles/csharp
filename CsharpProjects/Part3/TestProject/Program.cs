2b421583fc76de68c9abc61a5144d8a5170aaee9
mandi andrade <mandi.andrade20@gmail.com>
2024-02-02 18:54:39 -0600

    refactor if else statements to not be on one line for readability

--- CsharpProjects/Part3/TestProject/Program.cs
+++ CsharpProjects/Part3/TestProject/Program.cs
@@ -324,8 +324,12 @@ Console.WriteLine("*** FizzBuzz challenge ***");
 
 for (int i = 1; i < 101; i++)
 {
-  if (i % 15 == 0) Console.WriteLine($"{i} - FizzBuzz");
-  else if (i % 5 == 0) Console.WriteLine($"{i} - Buzz");
-  else if (i % 3 == 0) Console.WriteLine($"{i} - Fizz");
-  else Console.WriteLine(i);
+  if (i % 15 == 0)
+    Console.WriteLine($"{i} - FizzBuzz");
+  else if (i % 5 == 0)
+    Console.WriteLine($"{i} - Buzz");
+  else if (i % 3 == 0)
+    Console.WriteLine($"{i} - Fizz");
+  else
+    Console.WriteLine(i);
 }
