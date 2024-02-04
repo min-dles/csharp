a93b467ca62724259a3b280ae4209b8fb2a94549
mandi andrade <mandi.andrade20@gmail.com>
2024-02-03 20:23:59 -0600

    practice with continue keyword

--- CsharpProjects/Part3/TestProject/Program.cs
+++ CsharpProjects/Part3/TestProject/Program.cs
@@ -340,17 +340,21 @@ Console.WriteLine("*** Module 5 ***");
 
 Random random = new Random();
 int current = random.Next(1, 11);
-/*
+
 do
 {
   current = random.Next(1, 11);
-  Console.WriteLine(current);
-} while (current != 7); // the loop will end when the random integer lands on 7 
-*/
 
+  if (current >= 8) continue;
+
+  Console.WriteLine(current);
+} while (current != 7); 
+
+/*
 while (current >= 3)
 {
   Console.WriteLine(current);
   current = random.Next(1, 11);
 }
 Console.WriteLine($"Last number: {current}");
+*/
