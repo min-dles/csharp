2a9e97e906ae4b3a56ac46a70b694944fdb70740
mandi andrade <mandi.andrade20@gmail.com>
2024-02-07 20:43:41 -0600

    loop through ourAnimals array to find missing animalAge data

--- CsharpProjects/Part3/GuidedProject/Starter/Program.cs
+++ CsharpProjects/Part3/GuidedProject/Starter/Program.cs
@@ -269,7 +269,36 @@ do
 
         case "3":
             // Ensure animal ages and physical descriptions are complete
-            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            // 1. Be enabled inside the appropriate application branch (must not overwrite the code in the code branch for menu option 2).
+
+            // 2. Skip over any animal in the ourAnimals array when the value of pet ID is set to the default value.
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    readResult = Console.ReadLine();
+                    if (ourAnimals[i, 2] == "Age: ?")
+                    {
+                        Console.WriteLine($"Enter an age for {ourAnimals[i, 0]}.");
+                    }
+                }
+            }
+
+            // 3. Display the pet ID value and prompt the user for an updated data value if ourAnimals array data is missing or incomplete.
+
+            // 4. Ensure that a valid numeric value is assigned to animalAge for all animals in the ourAnimals array that have assigned data.
+
+            // 5. Ensure that a valid string is assigned to animalPhysicalDescription for all animals in the ourAnimals array that have assigned data.
+
+            // 6. Enforce the following validation rules for animalAge.
+                // It must be possible to convert the value entered to numeric data type.
+
+            // 7. Enforce the following validation rules for animalPhysicalDescription:
+                // Values cannot be null.
+                // Values cannot have zero characters.
+                // Any further restriction is up to the developer.
+
+            // 8. Inform the application user when all data requirements are met, pausing the application to ensure the message can be seen and responded to.
             Console.WriteLine("Press the Enter key to continue");
             readResult = Console.ReadLine();
             break;
