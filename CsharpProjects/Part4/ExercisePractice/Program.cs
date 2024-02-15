dfa64666c04ddd76e08d894157c265222828a356
mandi andrade <mandi.andrade20@gmail.com>
2024-02-15 17:01:07 -0600

    test Clear() method and null values

--- CsharpProjects/Part4/ExercisePractice/Program.cs
+++ CsharpProjects/Part4/ExercisePractice/Program.cs
@@ -144,3 +144,15 @@ foreach (var pallet in pallets)
 {
   Console.WriteLine($"-- {pallet}");
 }
+
+Console.WriteLine($"\nBefore: {pallets[0].ToLower()}");
+Array.Clear(pallets, 0, 2); // in pallets array, start at index 0 and clear 2 elements
+if (pallets[0] != null)
+  Console.WriteLine($"After: {pallets[0].ToLower()}");
+else
+  Console.WriteLine("After: null");
+Console.WriteLine($"\nClearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+  Console.WriteLine($"-- {pallet}");
+}
