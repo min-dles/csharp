55541c9c8b7daf32f2be230b9551fe5cecfe4cbd
mandi andrade <mandi.andrade20@gmail.com>
2024-02-15 17:07:50 -0600

    Resize() to make array larger

--- CsharpProjects/Part4/ExercisePractice/Program.cs
+++ CsharpProjects/Part4/ExercisePractice/Program.cs
@@ -145,13 +145,19 @@ foreach (var pallet in pallets)
   Console.WriteLine($"-- {pallet}");
 }
 
-Console.WriteLine($"\nBefore: {pallets[0].ToLower()}");
 Array.Clear(pallets, 0, 2); // in pallets array, start at index 0 and clear 2 elements
-if (pallets[0] != null)
-  Console.WriteLine($"After: {pallets[0].ToLower()}");
-else
-  Console.WriteLine("After: null");
 Console.WriteLine($"\nClearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+  Console.WriteLine($"-- {pallet}");
+}
+
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"\nResizing to 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
 foreach (var pallet in pallets)
 {
   Console.WriteLine($"-- {pallet}");
