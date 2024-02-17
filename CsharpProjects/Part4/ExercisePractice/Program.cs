dab50b001befd0b5b44d2bacb8f8370c9ee9d8e5
mandi andrade <mandi.andrade20@gmail.com>
2024-02-16 20:34:46 -0600

    swap number tokens and Format() for string interpolation

--- CsharpProjects/Part4/ExercisePractice/Program.cs
+++ CsharpProjects/Part4/ExercisePractice/Program.cs
@@ -216,7 +216,6 @@
 // Module 4 - Format alphanumeric data for presentation in C#
 string first = "Hello";
 string second = "World";
-string result = string.Format("{0} {1}!", first, second);
-Console.WriteLine(result); // Hello World!
-Console.WriteLine("{1} {0}!", first, second); // World Hello!
-Console.WriteLine("{0} {0} {0}!", first, second); // O: Hello Hello Hello!
+Console.WriteLine($"{first} {second}"); // Hello World!
+Console.WriteLine($"{second} {first}!"); // World Hello!
+Console.WriteLine($"{first} {first} {first}!"); // O: Hello Hello Hello!
