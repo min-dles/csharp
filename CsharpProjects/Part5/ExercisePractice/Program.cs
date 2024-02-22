6b56540d9bc74d6ef527013747b528d5d8c16962
mandi andrade <mandi.andrade20@gmail.com>
2024-02-22 15:28:06 -0600

    initial commit for part 5

---
+++ CsharpProjects/Part5/ExercisePractice/Program.cs
@@ -0,0 +1,14 @@
+// Module 1 - Write your first C# Method
+int[] a = {1,2,3,4,5};
+
+Console.WriteLine("Contents of Array:");
+PrintArray();
+
+void PrintArray()
+{
+    foreach (int x in a)
+    {
+        Console.Write($"{x} ");
+    }
+    Console.WriteLine();
+}
