2037a733cda9663dc478e520b723237e9d137a42
mandi andrade <mandi.andrade20@gmail.com>
2024-02-22 21:40:36 -0600

    refactor code to use global var and test data

--- CsharpProjects/Part5/ExercisePractice/Program.cs
+++ CsharpProjects/Part5/ExercisePractice/Program.cs
@@ -95,34 +95,36 @@ then ipAddress is valid
 
 else ipAddress is invalid
 */
-string ipv4Input = "108.33.1.230";
+string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+string[] address;
 bool validLength = false;
 bool validZeroes = false;
 bool validRange = false;
 
-ValidateLength();
-ValidateZeroes();
-ValidateRange();
+foreach (string ip in ipv4Input)
+{
+  address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+  ValidateLength();
+  ValidateZeroes();
+  ValidateRange();
 
-if (validLength && validZeroes && validRange)
-{
-  Console.WriteLine($"ip is a valid IPv4 address.");
-}
-else 
-{
-  Console.WriteLine($"ip is *NOT* a valid IPv4 address.");
-}
+  if (validLength && validZeroes && validRange)
+  {
+    Console.WriteLine($"{ip} is a valid IPv4 address.");
+  }
+  else 
+  {
+    Console.WriteLine($"{ip} is *NOT* a valid IPv4 address.");
+  }
+};
 
 void ValidateLength() 
 {
-  string[] address = ipv4Input.Split(".");
   validLength = address.Length == 4;
 };
 
 void ValidateZeroes() 
 {
-  string[] address = ipv4Input.Split(".");
-
   foreach (string number in address)
   {
     if (number.Length > 1 && number.StartsWith("0"))
@@ -136,8 +138,6 @@ void ValidateZeroes()
 
 void ValidateRange() 
 {
-  string[] address = ipv4Input.Split(".", StringSplitOptions.RemoveEmptyEntries);
-
   foreach (string number in address)
   {
     int value = int.Parse(number);
