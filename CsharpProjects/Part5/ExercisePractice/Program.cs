4a5e25ad96fe7fcd4ec9df00283c194625103f8a
mandi andrade <mandi.andrade20@gmail.com>
2024-02-29 13:08:25 -0600

    iterate thru for loop to call method and update total variable

--- CsharpProjects/Part5/ExercisePractice/Program.cs
+++ CsharpProjects/Part5/ExercisePractice/Program.cs
@@ -394,6 +394,11 @@ double minimumSpend = 30.00;
 double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
 double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
 
+for (int i = 0; i < items.Length; i++)
+{
+  total += GetDiscountedPrice(i);
+}
+
 Console.WriteLine($"Total: ${total}");
 
 double GetDiscountedPrice(int itemIndex)
