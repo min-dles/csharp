9119946e3e14884210fae6f45683e3c322e6570a
mandi andrade <mandi.andrade20@gmail.com>
2024-02-29 18:26:36 -0600

    initialize exercise 3 to return string value

--- CsharpProjects/Part5/ExercisePractice/Program.cs
+++ CsharpProjects/Part5/ExercisePractice/Program.cs
@@ -420,20 +420,27 @@ using System;
   }
 
 // Exercise 2: return numbers from methods (currency exchange example)
-double usd = 23.73;
-int vnd = UsdToVnd(usd);
+  double usd = 23.73;
+  int vnd = UsdToVnd(usd);
 
-Console.WriteLine($"${usd} USD = ${vnd} VND");
-Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD"); // can make the method call directly in the WriteLine
+  Console.WriteLine($"${usd} USD = ${vnd} VND");
+  Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD"); // can make the method call directly in the WriteLine
 
-int UsdToVnd(double usd)
+  int UsdToVnd(double usd)
+  {
+    int rate = 23500; // notice rate type is int here...
+    return (int) (rate * usd);
+  }
+
+  double VndToUsd(int vnd)
+  {
+    double rate = 23500; // ... and double type here, to match return value
+    return vnd / rate;
+  }
+
+// Exercise 3: Return string values
+string ReverseWord(string word)
 {
-  int rate = 23500; // notice rate type is int here...
-  return (int) (rate * usd);
-}
-
-double VndToUsd(int vnd)
-{
-  double rate = 23500; // ... and double type here, to match return value
-  return vnd / rate;
+  string result = "";
+  return result;
 }
