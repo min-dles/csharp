0b76746978c08c59d360404f9ddddb6531fd2eea
mandi andrade <mandi.andrade20@gmail.com>
2024-03-01 17:57:06 -0600

    pseudocode initial steps for guidedproj

--- CsharpProjects/Part5/GuidedProject/Program.cs
+++ CsharpProjects/Part5/GuidedProject/Program.cs
@@ -6,3 +6,17 @@ string[] pettingZoo =
   "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
   "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+void RandomizeAnimals(string[] zooAnimals)
+{
+  // this method will pick animals randomly
+};
+
+// string[,] group = AssignGroup();
+
+Console.WriteLine("School A");
+
+void PrintGroup(string group)
+{
+  // method to print the animal groups
+};
