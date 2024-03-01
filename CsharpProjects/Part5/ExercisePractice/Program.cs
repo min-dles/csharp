60cee55036b09a3b40c829b842f22b8c97bc011a
mandi andrade <mandi.andrade20@gmail.com>
2024-02-29 18:16:01 -0600

    initialize exercise 2 currency exchange method

--- CsharpProjects/Part5/ExercisePractice/Program.cs
+++ CsharpProjects/Part5/ExercisePractice/Program.cs
@@ -387,33 +387,45 @@ using System;
       }
     }
 */
-// Module 3 - PLACEHOLDER
-double total = 0;
-double minimumSpend = 30.00;
+// Module 3 - Create methods that return values
+// Exercise 1: First practice with return types in methods
+  double total = 0;
+  double minimumSpend = 30.00;
 
-double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
-double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
+  double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
+  double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
 
-for (int i = 0; i < items.Length; i++)
+  for (int i = 0; i < items.Length; i++)
+  {
+    total += GetDiscountedPrice(i);
+  }
+
+  total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+  Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+  double GetDiscountedPrice(int itemIndex)
+  {
+    return items[itemIndex] * (1 - discounts[itemIndex]);
+  }
+
+  bool TotalMeetsMinimum()
+  {
+    return total >= minimumSpend;
+  }
+
+  string FormatDecimal(double input)
+  {
+    return input.ToString().Substring(0, 5);
+  }
+
+// Exercise 2: return numbers from methods (currency exchange example)
+double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+
+int UsdToVnd(double usd)
 {
-  total += GetDiscountedPrice(i);
-}
-
-total -= TotalMeetsMinimum() ? 5.00 : 0.00;
-
-Console.WriteLine($"Total: ${FormatDecimal(total)}");
-
-double GetDiscountedPrice(int itemIndex)
-{
-  return items[itemIndex] * (1 - discounts[itemIndex]);
-}
-
-bool TotalMeetsMinimum()
-{
-  return total >= minimumSpend;
-}
-
-string FormatDecimal(double input)
-{
-  return input.ToString().Substring(0, 5);
+  // this method will convert the rate (double to int, following rate information)
 }
