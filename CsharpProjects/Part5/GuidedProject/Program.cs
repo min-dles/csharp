ae7dcaad395d04c4ccdaa9c5d42bcfe3ef3f9ed0
mandi andrade <mandi.andrade20@gmail.com>
2024-03-01 18:04:12 -0600

    now loop through the array

--- CsharpProjects/Part5/GuidedProject/Program.cs
+++ CsharpProjects/Part5/GuidedProject/Program.cs
@@ -12,12 +12,14 @@ void RandomizeAnimals(string[] zooAnimals)
   // this method will pick animals randomly
   Random random = new Random();
 
-  int i = 0;
-  int r = random.Next(pettingZoo.Length);
+  for (int i = 0; i < pettingZoo.Length; i++)
+  {
+    int r = random.Next(pettingZoo.Length);
 
-  string temp = pettingZoo[i];
-  pettingZoo[i] = pettingZoo[r];
-  pettingZoo[r] = temp;
+    string temp = pettingZoo[i];
+    pettingZoo[i] = pettingZoo[r];
+    pettingZoo[r] = temp;
+  }
 };
 
 // string[,] group = AssignGroup();
