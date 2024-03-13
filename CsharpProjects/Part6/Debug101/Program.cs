cd5b458394a9bd59104e5a034f53dd965134d722
mandi andrade <mandi.andrade20@gmail.com>
2024-03-13 17:40:01 -0500

    practice with WATCH and editing VARIABLES

--- CsharpProjects/Part6/Debug101/Program.cs
+++ CsharpProjects/Part6/Debug101/Program.cs
@@ -32,7 +32,7 @@ static string AndrewMessage()
 }
 */
 
-// EXERCISE 2: 
+/* EXERCISE 2: 
 string? readResult;
 int startIndex = 0;
 bool goodEntry = false;
@@ -77,3 +77,17 @@ static int SumValues(int[] numbers, int n)
     }
     return sum;
 }
+*/
+
+// EXERCISE 3:
+bool exit = false;
+var rand = new Random();
+int num1 = 5;
+int num2 = 5;
+
+do
+{
+    num1 = rand.Next(1, 11);
+    num2 = num1 + rand.Next(1, 51);
+
+} while (exit == false);
