9101cfe3b0398d00e50140525e317a71e0604849
mandi andrade <mandi.andrade20@gmail.com>
2024-03-19 14:00:59 -0500

    catch multiple exception types practice

--- CsharpProjects/Part6/ExercisePractice/Program.cs
+++ CsharpProjects/Part6/ExercisePractice/Program.cs
@@ -77,7 +77,7 @@ static void Process3(string[,] products, int item)
 
 // Module Three: Implement Exception Handling in C# console apps
 
-// implement a simple try-catch
+/* implement a simple try-catch
 try
 {
   TestCallStack();
@@ -122,5 +122,31 @@ static void WriteMessage()
   checked
   {
     smallNumber = (byte)number1;
+  }
+}
+*/
+
+// catch separate exception types
+// inputValues is used to store numeric values entered by a user
+string[] inputValues = new string[]{"three", "9999999999", "0", "2" };
+
+foreach (string inputValue in inputValues)
+{
+  int numValue = 0;
+  try
+  {
+    numValue = int.Parse(inputValue);
+  }
+  catch (FormatException)
+  {
+    Console.WriteLine("Invalid readResult. Please enter a valid number.");
+  }
+  catch (OverflowException)
+  {
+    Console.WriteLine("The number you entered is too large or too small.");
+  }
+  catch(Exception ex)
+  {
+    Console.WriteLine(ex.Message);
   }
 }
