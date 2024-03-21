c7f159b22f8355e9b386a1f77e9debda6cee43f8
mandi andrade <mandi.andrade20@gmail.com>
2024-03-21 13:10:14 -0500

    review solution to exercise in debugger

--- CsharpProjects/Part6/ExercisePractice/Program.cs
+++ CsharpProjects/Part6/ExercisePractice/Program.cs
@@ -293,6 +293,7 @@ Console.Write("Enter the upper bound: ");
 int upperBound = int.Parse(Console.ReadLine());
 
 decimal averageValue = 0;
+bool exit = false;
 
 do
 {
@@ -303,16 +304,27 @@ do
 
     // Display the value returned by AverageOfEvenNumbers in the console
     Console.WriteLine($"The average of even numbers between {lowerBound} and {upperBound} is {averageValue}.");
+
+    exit = true;
   }
   catch (ArgumentOutOfRangeException ex)
   {
     Console.WriteLine("An error has occurred.");
     Console.WriteLine(ex.Message);
     Console.WriteLine($"The upper bound must be greater than {lowerBound}");
-    Console.Write($"Enter a new upper bound: ");
-    upperBound = int.Parse(Console.ReadLine());
+    Console.Write($"Enter a new upper bound (or type 'exit' to quit the program): ");
+    string? userResponse = Console.ReadLine();
+    if (userResponse.ToLower().Contains("exit"))
+    {
+      exit = true;
+    }
+    else 
+    {
+      exit = false;
+      upperBound = int.Parse(userResponse);
+    }
   }
-}
+} while (exit == false);
 
 
 // Wait for user input
