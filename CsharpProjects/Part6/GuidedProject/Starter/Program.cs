6c6b6b9b51c8861f6ef47e1d64bab38f8471d347
mandi andrade <mandi.andrade20@gmail.com>
2024-03-28 17:08:36 -0500

    fix logic error and pass tests in debugger

--- CsharpProjects/Part6/GuidedProject/Starter/Program.cs
+++ CsharpProjects/Part6/GuidedProject/Starter/Program.cs
@@ -140,7 +140,7 @@ static string MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, i
 
     while ((changeNeeded > 4) && (cashTill[1] > 0))
     {
-        cashTill[2]--;
+        cashTill[1]--;
         changeNeeded -= 5;
         Console.WriteLine("\t A five");
     }
